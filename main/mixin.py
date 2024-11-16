3b630b77c7a8339570701fc967d1f8c4271da73c
DISvep <321861bro@gmail.com>
2024-11-16 19:11:02 +0200

    comments, reg, login, likes and some more

---
+++ main/mixin.py
@@ -0,0 +1,19 @@
+from django.core.exceptions import PermissionDenied
+
+
+class UserIsOwnerMixin(object):
+    def dispatch(self, request, *args, **kwargs):
+        Task = self.get_object()
+        if Task.user != self.request.user:
+            raise PermissionDenied
+        else:
+            return super().dispatch(request, *args, **kwargs)
+
+
+class UserIsCommentOwnerMixin(object):
+    def dispatch(self, request, *args, **kwargs):
+        Comment = self.get_object()
+        if Comment.author != self.request.user:
+            raise PermissionDenied
+        else:
+            return super().dispatch(request, *args, **kwargs)
--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate
+from django.contrib.auth import views as auth_views
+from .views import TaskList, TaskDetail, TaskCreate, TaskDelete, TaskUpdate, register, CommentDelete, CommentUpdate, LikeView, DislikeView
 
 
 urlpatterns = [
-    path("tasks/", TaskList.as_view(), name="task-list"),
-    path("tasks/task/<int:pk>/", TaskDetail.as_view(), name="task-detail"),
-    path("tasks/create/", TaskCreate.as_view(), name="task-create")
+    path("", TaskList.as_view(), name="task-list"),
+    path("task/<int:pk>/", TaskDetail.as_view(), name="task-detail"),
+    path("task/create/", TaskCreate.as_view(), name="task-create"),
+    path('task/delete/<int:pk>', TaskDelete.as_view(), name='task-delete'),
+    path('task/update/<int:pk>', TaskUpdate.as_view(), name='task-update'),
+    path('comment/delete/<int:pk>', CommentDelete.as_view(), name='comment-delete'),
+    path('comment/update/<int:pk>', CommentUpdate.as_view(), name='comment-update'),
+    path('register/', register, name='register'),
+    path('like/<int:pk>', LikeView.as_view(), name='like_comment'),
+    path('dislike/<int:pk>', DislikeView.as_view(), name='dislike_comment')
 ]
